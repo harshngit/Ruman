b8ba6148a7ffade54d3c98a98b7cadd2ee024a2d
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-01-13 13:52:10 +0530

    Image Change

--- src/components/Home/HomeBanner.tsx
+++ src/components/Home/HomeBanner.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import mobile from '../../../public/images/banner/Mobile_Account.png'
 import Image from 'next/image'
-import pc from '../../../public/images/banner/A C C O U N T.png'
+import pc from '../../../public/images/banner/A C C O U N T (2).png'
 const HomeBanner = () => {
 	return (
 		<>
