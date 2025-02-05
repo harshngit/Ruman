036c9c434e3083025e9557b50ea20915e1b26cf9
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-02-05 09:29:36 +0530

    Google Any

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/style.scss";
 import Head from "next/head";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <GoogleTagManager gtmId="G-CZ1CKKJML1" />
         <link rel="icon" href="images/rumanfavicon (1).png" type="image/png" />
       </head>
       <body className={inter.className}>{children}</body>
