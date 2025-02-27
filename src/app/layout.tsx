1708b38719d81d626bc7b8d6a1c03abd371fd906
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-02-27 16:14:50 +0530

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,49 @@ import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Ruman Accounting Services | Expert Family Accounting & Smart Financial Solutions",
+  description: "Trust Ruman Accounting for expert family accounting and financial intelligence solutions.",
+  keywords: ['Ruman Accounting Services', 'Accounting Intelligence', 'Family Accounting', 'Accouting Services', 'Accounting', 'Accounting in Mumbai', ' Accounting in Pune', "Accounting in Thane", 'Accounting for startups'],
+  verification: {
+    google: 'GgyyKHIryfr_5yxyEl0so5VwdH3xU0rPUZwKv310XNQ',
+  },
+  openGraph: {
+    type: "website",
+    title: 'Ruman Accounting Services | Expert Family Accounting & Smart Financial Solutions',
+    description: 'Trust Ruman Accounting for expert family accounting and financial intelligence solutions.',
+    url: 'https://rumanaccounting.com/',
+    siteName: 'Ruman Accounting Services | Expert Family Accounting & Smart Financial Solutions',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Ruman Accounting – Your Partner in Family Accounting & Smart Financial Intelligence.',
+      },
+    ],
+
+  },
+  alternates: {
+    canonical: 'https://rumanaccounting.com/',
+  },
+
+  icons: {
+    icon: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742',
+    shortcut: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742',
+    apple: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: 'https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/log%2Frumanfavicon%20(1).png?alt=media&token=92f2b9d7-e9c1-4d25-ba9b-9d8e88f64742',
+    },
+  },
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
