10841fd23caec59a3425e5d495c85b9afa395ecb
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-03-02 13:59:06 +0530

    Hot jar 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,9 @@ import "@/styles/style.scss";
 import Head from "next/head";
 import { GoogleTagManager } from '@next/third-parties/google'
 import Script from "next/script";
-import Hotjar from '@hotjar/browser';
+
 
 const inter = Inter({ subsets: ["latin"] });
-const siteId = 5323136;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 export const metadata = {
   title: "Professional Accounting Services | Trusted CA Firm - Ruman Accounting Services",
   description: "Trust Ruman Accounting for expert family accounting and financial intelligence solutions.",
@@ -64,18 +60,18 @@ export default function RootLayout({
         <GoogleTagManager gtmId="G-CZ1CKKJML1" />
 
         <link rel="icon" href="images/rumanfavicon (1).png" type="image/png" />
-        {/* <Script id="HotJarAnalytics" >
+        <Script id="HotJarAnalytics" >
           {
             `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5292471,hjsv:6};
+        h._hjSettings={hjid:5323136,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`}
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
 
-        </Script> */}
+        </Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
